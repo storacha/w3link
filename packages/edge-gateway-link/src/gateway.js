77ce8a88bdd0f072a61e7bdee5009a07640b7b63
Vasco Santos <santos.vasco10@gmail.com>
2022-09-06 10:08:17 +0200

    fix: redirect to product page

--- packages/edge-gateway-link/src/gateway.js
+++ packages/edge-gateway-link/src/gateway.js
@@ -1,5 +1,7 @@
 /* eslint-env serviceworker, browser */
 
+const PRODUCT_URL = 'https://web3.storage/products/w3link/'
+
 /**
  * Handle gateway request
  *
@@ -7,6 +9,14 @@
  * @param {import('./env').Env} env
  */
 export async function gatewayGet (request, env) {
+  // Redirect to product page
+  if (!request.url.includes('ipfs') && !request.url.includes('ipns')) {
+    return Response.redirect(
+      PRODUCT_URL,
+      302
+    )
+  }
+
   // Redirect if ipns
   if (request.url.includes(env.IPNS_GATEWAY_HOSTNAME)) {
     return Response.redirect(
